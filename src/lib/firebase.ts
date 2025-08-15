ab71d8c16093afcf240918fb9124c3b224ab7107
pakalogi01-lang <pakalogi01@gmail.com>
2025-08-15 10:15:36 +0400

    Fix deployment error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getStorage } from "firebase/storage";
@@ -6,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDGJbhspksbAiunk9RkIhl3ODMSJRxbmXg",
-  authDomain: "cargoconnect-m4v8q.firebaseapp.com",
-  projectId: "cargoconnect-m4v8q",
-  storageBucket: "cargoconnect-m4v8q.firebasestorage.app",
-  messagingSenderId: "850958660859",
-  appId: "1:850958660859:web:06eeb79184a6180e4c2c5e"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
