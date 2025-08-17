d4e3e0ef7ca7e9879c47f3342ca485307cd096ab
pakalogi01-lang <pakalogi01@gmail.com>
2025-08-17 04:21:02 +0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { NotificationProvider } from '@/context/NotificationContext';
 
 export const metadata: Metadata = {
-  title: 'SwiftHaul',
+  title: 'BURAQFLEET',
   description: 'Connecting traders and drivers seamlessly.',
 };
 
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
