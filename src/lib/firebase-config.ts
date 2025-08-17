6805499cdf36ea6b370ca2f04f0c7edf0b978054
pakalogi01-lang <pakalogi01@gmail.com>
2025-08-17 04:17:09 +0400

    Create firebase-config.ts

---
+++ src/lib/firebase-config.ts
@@ -0,0 +1,9 @@
+// This file contains the configuration object for connecting to your Firebase project.
+export const firebaseConfig = {
+  apiKey: "AIzaSyDGJbhspksbAiunk9RkIhl3ODMSJRxbmXg",
+  authDomain: "cargoconnect-m4v8q.firebaseapp.com",
+  projectId: "cargoconnect-m4v8q",
+  storageBucket: "cargoconnect-m4v8q.appspot.com",
+  messagingSenderId: "850958660859",
+  appId: "1:850958660859:web:06eeb79184a6180e4c2c5e"
+};
